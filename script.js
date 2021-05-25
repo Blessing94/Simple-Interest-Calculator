6b52e1d8d415c16c90c5c7c2982e827957d213e2
Blessing94 <63746633+Blessing94@users.noreply.github.com>
2021-05-26 00:46:54 +0800

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = Number(document.getElementById("principal").value);
+    // Validation, only positive numbers
+    if (principal = "" || principal <= 0){
+        alert("Enter a positive number")
+        document.getElementById("principal").focus()
+        return
+    }
+    rate = Number(document.getElementById("rate").value);
+    years = Number(document.getElementById("years").value);
+    // calculating interest
+    interest = principal * years * rate /100;
+    // the new year for the interest
+    new_year=(Number(new Date().getFullYear())+years);
+    // when you click "compute interest"
+    document.getElementById("result").innerHTML="If you deposit <span class='var'>"+principal+"</span>,<br>at an interest rate of <span class='var'>"+ rate +"</span>%.<br>You will receive an amount of <span class='var'>"+interest+"</span>,<br> in the year <span class='var'>"+new_year+"</span>"
 }
-        
+function range_change(){
+    document.getElementById("num_rate").innerHTML=document.getElementById("rate").value+"%"
+}
+    
